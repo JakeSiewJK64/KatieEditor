d344a2fdc58e3ef727b5a0c21e8e6e9289394cf2
JakeSiewJK64 <joekanesiew@outlook.com>
2025-05-12 20:14:21 +0800

    add serialize html example

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,31 @@ function DeserializeHtml() {
         <p>Preview: </p>
         {value.length > 0 && (
           <pre className="h-[10rem] overflow-y-scroll border border-slate-300 p-2">
-            {JSON.stringify(deserialize(value), null, 2)}
+            {JSON.stringify(value, null, 2)}
+          </pre>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function SerializeHtml() {
+  const [value, setValue] = React.useState('');
+
+  return (
+    <div>
+      <RichTextEditor
+        mode="html"
+        onSave={setValue}
+        initialValue={deserialize(
+          '<img src="https://th.bing.com/th?id=ORMS.619350583d28c0fdd02328c89273585a&pid=Wdp&w=240&h=129&qlt=90&c=1&rs=1&dpr=0.800000011920929&p=0" />',
+        )}
+      />
+      <div className="p-2">
+        <p>Preview: </p>
+        {value.length > 0 && (
+          <pre className="h-[10rem] overflow-y-scroll border border-slate-300 p-2">
+            {value}
           </pre>
         )}
       </div>
@@ -47,6 +71,8 @@ function App() {
       <Example />
       <hr />
       <DeserializeHtml />
+      <hr />
+      <SerializeHtml />
       <hr />
     </div>
   );
