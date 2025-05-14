7162daf92c12c7a78ef1a232a6301d5b3905cc4a
JakeSiewJK64 <joekanesiew@outlook.com>
2025-05-14 11:17:48 +0800

    add remove image button

--- editor/editorElements.tsx
+++ editor/editorElements.tsx
@@ -1,3 +1,13 @@
+import { ImageElement } from 'editor';
+import { LuTrash } from 'react-icons/lu';
+import { Transforms } from 'slate';
+import {
+  ReactEditor,
+  useFocused,
+  useSelected,
+  useSlateStatic,
+} from 'slate-react';
+
 import { cn } from './utils';
 
 import type { RenderElementProps, RenderLeafProps } from 'slate-react';
@@ -13,7 +23,38 @@ export function CustomEditorLeaf(props: RenderLeafProps) {
   );
 }
 
+function CustomImageRenderElement({
+  onDelete = () => {},
+  ...props
+}: ImageElement & { onDelete: () => void }) {
+  const selected = useSelected();
+  const focused = useFocused();
+
+  return (
+    <div
+      className={cn(
+        'w-fit rounded border border-slate-300 p-2',
+        focused && selected && 'border-red-600',
+      )}
+      contentEditable={false}
+    >
+      <div className="mb-1 flex flex-row items-center gap-1">
+        <button
+          title="Remove image"
+          className="cursor-pointer rounded border border-slate-300 p-1 text-red-600"
+          onClick={onDelete}
+        >
+          <LuTrash />
+        </button>
+      </div>
+      <img draggable={false} src={props.url} />
+    </div>
+  );
+}
+
 export function CustomEditorRenderElement(props: RenderElementProps) {
+  const editor = useSlateStatic();
+
   switch (props.element.type) {
     case 'code':
       return (
@@ -22,7 +63,16 @@ export function CustomEditorRenderElement(props: RenderElementProps) {
         </pre>
       );
     case 'image':
-      return <img src={props.element.url} />;
+      return (
+        <CustomImageRenderElement
+          {...props.element}
+          onDelete={() => {
+            Transforms.removeNodes(editor, {
+              at: ReactEditor.findPath(editor, props.element),
+            });
+          }}
+        />
+      );
     default:
       return <p {...props}>{props.children}</p>;
   }
